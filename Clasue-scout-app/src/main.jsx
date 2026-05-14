8fd656811adab02653a922263dfd7b8f69b2603e
Janik <janikdhadwar081@gmail.com>
2026-05-14 15:49:40 -0700

    same as last one

--- Clasue-scout-app/src/main.jsx
+++ Clasue-scout-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './frc-scout-app-FireB.jsx' //For Online DataBase Saving(FireBase)
-// import App from './frc-scout-app-Local.jsx' //For Local File Saving (Self Hosting)
+// import App from './frc-scout-app-FireB.jsx' //For Online DataBase Saving(FireBase)
+import App from './frc-scout-app-local.jsx' //For Local File Saving (Self Hosting)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
