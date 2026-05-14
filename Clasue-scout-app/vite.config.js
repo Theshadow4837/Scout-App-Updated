cd610f3b55b8501f59fcab3474020affe34051a8
Janik Dhadwar <166580505+Theshadow4837@users.noreply.github.com>
2026-05-13 20:00:06 -0700

    Update vite.config.js

--- Clasue-scout-app/vite.config.js
+++ Clasue-scout-app/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+server: {
+    host: '0.0.0.0',
+    allowedHosts: ['server.tail4582.ts.net']
+  }
   plugins: [
     react(),
     VitePWA({
